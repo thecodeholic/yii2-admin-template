93de724d88790b054ca51fe1e9a331463b6076d1
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-04-26 13:13:02 +0400

    Install dotenv and load values from .env file

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,21 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => getenv('DB_DSN'),
+            'username' => getenv('DB_USER'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset' => getenv('DB_CHARSET'),
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => getenv(''),
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
---
+++ common/env.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: TheCodeholic
+ * Date: 4/26/2020
+ * Time: 1:10 PM
+ */
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();

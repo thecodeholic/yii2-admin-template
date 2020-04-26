0969d00036ab1d3d2f07c8808852eaf7fcdf5b27
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-04-26 13:14:48 +0400

    Delete usages from main-local.php files

--- backend/web/index-test.php
+++ backend/web/index-test.php
@@ -15,11 +15,9 @@ require __DIR__ . '/../config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../../common/config/main-local.php',
     require __DIR__ . '/../../common/config/test.php',
     require __DIR__ . '/../../common/config/test-local.php',
     require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-local.php',
     require __DIR__ . '/../config/test.php',
     require __DIR__ . '/../config/test-local.php'
 );
--- backend/web/index.php
+++ backend/web/index.php
@@ -11,7 +11,6 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
     require __DIR__ . '/../../common/config/web.php',
     require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-local.php',
     require __DIR__ . '/../config/web.php'
 );
 

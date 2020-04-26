111b5b6d022b4b9ec7ba6b16dc2d7a6f2e980378
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-04-26 15:11:05 +0400

    Cleanup layout file

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -3,11 +3,6 @@
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
-$this->registerJsFile('/js/dashboard.js', [
-    'depends' => [
-        \backend\assets\AppAsset::class
-    ]
-]);
 ?>
 <div class="site-index">
     Dashboard

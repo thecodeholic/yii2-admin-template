95cf8126ce2f29c47471972f20a460850dd16e40
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-04-26 15:20:51 +0400

    Add i18n component

--- common/config/main.php
+++ common/config/main.php
@@ -2,7 +2,7 @@
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
@@ -24,5 +24,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                ],
+            ]
+        ]
     ],
 ];
